8908ee1e3f1b2d90ef08aa0d2f670c5634b75af1
saadshah16 <72925656+saadshah16@users.noreply.github.com>
2024-07-31 04:01:24 +0530

    Time: 574 ms (86.55%), Space: 20.7 MB (65.1%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,25 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
         nums.sort()
-        s = set()
-        for i in range(len(nums) - 2):
-            firstNum = nums[i]
-            j = i + 1
-            k = len(nums) - 1
-            while j < k:
-                secondNum  = nums[j]
-                thirdNum = nums[k]
+        res = []
 
-                potentialSum = firstNum + secondNum + thirdNum 
-                if potentialSum > 0:
-                    k -= 1
-                elif potentialSum < 0:
-                    j += 1
+        for i in range(n):
+            if nums[i] > 0:
+                break
+            elif i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            low,high = i+1,n-1
+            while low < high:
+                sum = nums[i] + nums[low] + nums[high]
+                if sum == 0:
+                    res.append([nums[i], nums[low], nums[high]])
+                    low += 1
+                    high -= 1
+                    while low < high and nums[low] == nums[low-1]:
+                        low += 1
+                    while low < high and nums[high] == nums[high+1]:
+                        high -= 1
+                elif sum < 0:
+                    low +=1
                 else:
-                    s.add((firstNum , secondNum ,thirdNum))
-                    j += 1
-                    k -= 1
-        return s
+                    high -= 1
+        return res
+            
     
     # TC: O(n^2)
     # SC: O(n)
