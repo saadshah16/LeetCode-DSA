70805b268719457355fa584a56db2971a1b80b01
saadshah16 <72925656+saadshah16@users.noreply.github.com>
2025-01-20 01:27:48 +0530

    Time: 13 ms (89.12%), Space: 18 MB (23.81%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,27 +1,22 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        n1 = len(s1)
-        n2 = len(s2)
         s1_counts = [0] * 26
         s2_counts = [0] * 26
-
-        if n1 > n2:
+        if len(s1) > len(s2):
             return False
 
-        for i in range(n1):
+        for i in range(len(s1)):
             s1_counts[ord(s1[i]) - 97] += 1
-            s2_counts[ord(s2[i]) - 97] += 1
+            s2_counts[ord(s2[i])- 97] += 1
 
-        if s1_counts == s2_counts:
-            return True
-
-        for i in range(n1, n2):
-            s2_counts[ord(s2[i]) - 97] += 1
-            s2_counts[ord(s2[i-n1]) - 97] -= 1
-            if s1_counts == s2_counts:
+        if  s2_counts == s1_counts:
                 return True
+        
+        for i in range(len(s1), len(s2)):
+            s2_counts[ord(s2[i]) - 97] += 1
+            s2_counts[ord(s2[i-len(s1)]) - ord('a')] -= 1
 
+            if s2_counts == s1_counts:
+                return True
+        
         return False
-
-    # TC: O(n)
-    # SC: O(1)
