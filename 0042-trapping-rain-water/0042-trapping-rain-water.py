4fd0b800c8e1bfe0611c2f4ca7acebe67332a12b
saadshah16 <72925656+saadshah16@users.noreply.github.com>
2025-01-28 00:20:39 -0500

    Time: 26 ms (31.79%), Space: 19.4 MB (30.97%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -4,20 +4,22 @@ class Solution:
         n = len(height)
         max_left = [0] * n
         max_right = [0] * n
-        
+
         for i in range(n):
             j = -i-1
             max_left[i] = l_wall
             max_right[j] = r_wall
-            l_wall = max(l_wall, height[i])
-            r_wall = max(r_wall, height[j])
-            
-        sum = 0
+            l_wall = max(l_wall,height[i])
+            r_wall = max(r_wall,height[j])
+        
+        summ = 0
+
         for i in range(n):
-            potential_water = min(max_left[i], max_right[i])
-            sum += max(0,potential_water - height[i])
-            
-        return sum
-    
-    #TC: O(n)
-    #SC: O(n)
+            pot_water = min(max_left[i],max_right[i])
+            summ += max(0,pot_water - height[i])
+        
+        return summ
+
+        
+
+       
