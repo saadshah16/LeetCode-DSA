4bd14468e7c1431cf4a8e99a3f73a37dc7820c57
saadshah16 <72925656+saadshah16@users.noreply.github.com>
2025-02-18 19:14:50 -0500

    Time: 1 ms (61.35%), Space: 19 MB (30.28%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,18 +1,31 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        hm = {}
-        n = len(nums)
-        k = 0
+        # Better solution using two pointers for o(1) sc
+        j = 0
 
-        for i in range(n):
-            if nums[i] not in hm.values():
-                hm[i] = nums[i]
-                nums[k] = nums[i]
-                k += 1
-            else:
-                continue
+        for i in range(1,len(nums)):
+            if nums[j] != nums[i]:
+                j+= 1
+                nums[j] = nums[i]
+        return j+1
+
+
+
+
+
+        #hm = {}
+        #n = len(nums)
+        #k = 0
+
+        #for i in range(n):
+         #   if nums[i] not in hm.values():
+          #      hm[i] = nums[i]
+           #     nums[k] = nums[i]
+            #    k += 1
+            #else:
+             #   continue
         
-        return k
+       # return k
 
             # TC: O(n)
             # SC: O(n)
