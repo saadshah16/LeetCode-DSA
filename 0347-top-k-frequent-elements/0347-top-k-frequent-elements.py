baf6d40aa51b102e46d4adfc49212bc7002ac533
saadshah16 <72925656+saadshah16@users.noreply.github.com>
2025-07-01 15:08:36 -0400

    Time: 11 ms (24.56%), Space: 24.1 MB (20.95%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,24 +1,23 @@
-from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        n = len(nums)
-        counter = Counter(nums)
-        buckets = [0] * (n + 1)
+        count = {} # Hashmap to count frequency
+        freq = [[] for i in range(len(nums)+1)] # Bucket array where the frequency == index
 
-        for num, freq in counter.items():
-            if buckets[freq] == 0:
-                buckets[freq] = [num]
-            else:
-                buckets[freq].append(num)
+        # Counting the frequency of each number
+        for num in nums:
+            count[num] = 1 + count.get(num,0)
         
-        ret = []
-        for i in range(n, -1, -1):
-            if buckets[i] != 0:
-                ret.extend(buckets[i])
-            if len(ret) == k:
-                break
+        # Grouping numbers by their frequency
+        for num,c in count.items():
+            freq[c].append(num)
         
-        return ret
-
-# TC: O(n)
-# SC: O(n)
+        res = []
+        # Traverse the bucket array in reverse to get the most frequent elements first
+        for i in range(len(nums),0,-1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+        
+        # TC: O(n)
+        # SC: O(n)
