81c8a3e2b78132f6b00d2b0c466e8af098f88720
saadshah16 <72925656+saadshah16@users.noreply.github.com>
2025-07-03 01:45:39 -0400

    Time: 3 ms (88.14%), Space: 19.3 MB (40.72%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,34 +1,43 @@
+from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        courses = prerequisites
+        # Step 1: Build the graph
         g = defaultdict(list)
-        for a,b in prerequisites:
+        for a,b in courses:
             g[a].append(b)
-            
+        
         UNVISITED = 0
         VISITING = 1
         VISITED = 2
-        
         states = [UNVISITED] * numCourses
-        
+
+        # DFS function to detect cycle
         def dfs(node):
             state = states[node]
-            if state == VISITED: return True
-            elif state == VISITING: return False
+            if state == VISITED:
+                return True # Already checked, no cylce here
+            elif state == VISITING:
+                return False # Found a cycle
             
+            # Mark course as visiting
             states[node] = VISITING
-            
+
+            # Visit all prerequisites of this course
             for nei in g[node]:
                 if not dfs(nei):
-                    return False
+                    return False # Found a cycle
+
+            # After all prereqs are safe, mark as visited
             states[node] = VISITED
             return True
         
-        
+
         for i in range(numCourses):
             if not dfs(i):
                 return False
         
         return True
-    
-    # TC: O(N+E)
-    # SC: O(N+E)
+
+        # TC: O(N+E) N = numCourses, E = total prereqs
+        # SC: O(N+E)
